c59cd490e3d0af673e4fe2933d2013fc793be5a0
MasahiroKanatani <5genbassnohito.maroro0208@gmail.com>
2021-02-18 10:41:43 +0900

    test

--- src/main/java/com/example/sandbox/SandboxApplication.java
+++ src/main/java/com/example/sandbox/SandboxApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SandboxApplication {
 
 	public static void main(String[] args) {
+		System.out.println("a");
 		SpringApplication.run(SandboxApplication.class, args);
 	}
 
